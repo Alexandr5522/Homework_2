c4801f89c06cba2788019a102b235161b676776b
Alexandr5522 <wodol604@gmail.com>
2024-10-12 18:07:06 +0300

    operators_if_elif_else

--- module_2_2.py
+++ module_2_2.py
@@ -1,10 +1,10 @@
-first = int(input('Введите число:'))                 # вводим 123 или 42 или 33
-second = int(input('Введите число:'))                # вводим 456 или 69 или 33
-third =  int(input('Введите число:'))                # вводим 789 или 42 или 33
-if first == second and first == third and second == third :
+first = int(input('Введите число:'))  # вводим 123 или 42 или 33
+second = int(input('Введите число:'))  # вводим 456 или 69 или 33
+third = int(input('Введите число:'))  # вводим 789 или 42 или 33
+if first == second and first == third and second == third:
     print('Все значения равны = 3')
-elif first == second or first == third or second == third or second == first :
+elif first == second or first == third or second == third or second == first:
     print('Только два из трех значений равны = 2/3')
 else:
     print('Значения не равны = 0')
-print('Проверка окончена')
+    print('Проверка окончена')
