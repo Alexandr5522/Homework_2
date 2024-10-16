efcfe7505542479400ed94df071b57ee7fac5894
Alexandr5522 <wodol604@gmail.com>
2024-10-16 19:47:48 +0300

    while

--- module_2_3.py
+++ module_2_3.py
@@ -1,11 +1,11 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0
 while index < len(my_list):
-    num = my_list[index]
-    if num == 0:
+    if my_list[index] == 0:
         index += 1
         continue
-    if num < 0:
+    elif my_list[index] > 0:
+        print('Значение: ',my_list[index])
+        index += 1
+    else:
         break
-    print('Значение: ',my_list[index])
-    index += 1
